db6f818d372715022b034cdddee13fd8efd02b90
Sirwq <vitaslav73@gmail.com>
2026-02-23 23:41:00 +0300

    trying to login

--- myProjects/gogoSpoty/main.go
+++ myProjects/gogoSpoty/main.go
@@ -1,24 +1,55 @@
 package main
 
 import (
-    "context"
-    "github.com/zmb3/spotify/v2"
-    spotifyauth "github.com/zmb3/spotify/v2/auth"
-    "golang.org/x/oauth2/clientcredentials"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
 )
 
-ctx := context.Background()
-config := &clientcredentials.Config{
-    ClientID:     "YOUR_CLIENT_ID",
-    ClientSecret: "YOUR_CLIENT_SECRET",
-    TokenURL:     spotifyauth.TokenURL,
-}
-
-token, _ := config.Token(ctx)
-httpClient := spotifyauth.New().Client(ctx, token)
-client := spotify.New(httpClient)
-
 func main() {
-	playing, err := client.PlayerCurrentlyPlaying(ctx)
-	fmt.Println(playing)
+	godotenv.Load(".env")
+
+	port := ":5111"
+
+	auth := spotifyauth.New(
+		spotifyauth.WithRedirectURL(os.Getenv("REDIRECT_URI")),
+		spotifyauth.WithScopes(
+			spotifyauth.ScopeUserReadPrivate,
+			spotifyauth.ScopePlaylistReadPrivate,
+		),
+		spotifyauth.WithClientID(os.Getenv("CLIENT_ID")),
+		spotifyauth.WithClientSecret(os.Getenv("CLIENT_SECRET")),
+	)
+
+	state := "randstate"
+
+	ch := make(chan *oauth2.Token)
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		token, err := auth.Token(r.Context(), state, r)
+		if err != nil {
+			http.Error(w, "Auth failed", http.StatusForbidden)
+			return
+		}
+		fmt.Fprintf(w, "Login success!")
+		ch <- token
+	})
+
+	go http.ListenAndServe(port, nil) // creating a server without handler
+
+	url := auth.AuthURL(state)
+	fmt.Println("Open this url:", url)
+
+	token := <-ch
+	client := spotify.New(auth.Client(context.Background(), token))
+
+	user, _ := client.CurrentUser(context.Background())
+	fmt.Println("Logged in as:", user.DisplayName)
+
 }
