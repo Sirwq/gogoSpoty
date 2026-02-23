8399322684de897b3a1d8e13b0d96c4ee93e13cb
Sirwq <vitaslav73@gmail.com>
2026-02-24 01:06:27 +0300

    saving token

--- myProjects/gogoSpoty/main.go
+++ myProjects/gogoSpoty/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,12 +17,14 @@ func main() {
 	godotenv.Load(".env")
 
 	port := ":5111"
+	redirUrl := "http://127.0.0.1:5111/callback"
 
 	auth := spotifyauth.New(
-		spotifyauth.WithRedirectURL(os.Getenv("REDIRECT_URI")),
+		spotifyauth.WithRedirectURL(redirUrl),
 		spotifyauth.WithScopes(
 			spotifyauth.ScopeUserReadPrivate,
 			spotifyauth.ScopePlaylistReadPrivate,
+			spotifyauth.ScopeUserReadCurrentlyPlaying,
 		),
 		spotifyauth.WithClientID(os.Getenv("CLIENT_ID")),
 		spotifyauth.WithClientSecret(os.Getenv("CLIENT_SECRET")),
@@ -43,13 +46,44 @@ func main() {
 
 	go http.ListenAndServe(port, nil) // creating a server without handler
 
-	url := auth.AuthURL(state)
-	fmt.Println("Open this url:", url)
+	token, err := loadToken()
+	if err != nil {
+		url := auth.AuthURL(state)
+		fmt.Println("Open this url: ", url)
+		token = <-ch
+		saveToken(token)
+	}
 
-	token := <-ch
 	client := spotify.New(auth.Client(context.Background(), token))
 
 	user, _ := client.CurrentUser(context.Background())
 	fmt.Println("Logged in as:", user.DisplayName)
 
+	playing, err := client.PlayerCurrentlyPlaying(context.Background())
+
+	if err != nil {
+		fmt.Println("Error while searching for playing", err)
+	}
+
+	fmt.Println(playing)
+
+}
+
+func saveToken(token *oauth2.Token) error {
+	data, err := json.Marshal(token)
+
+	if err != nil {
+		return err
+	}
+	return os.WriteFile("token.json", data, 0600)
+}
+
+func loadToken() (*oauth2.Token, error) {
+	data, err := os.ReadFile("token.json")
+	if err != nil {
+		return nil, err
+	}
+	var token oauth2.Token
+	err = json.Unmarshal(data, &token)
+	return &token, err
 }
